a8f79db878368005cc044cd4ebea3bfb2b521d04
Kuuhaku456 <manikfilip03@gmail.com>
2023-11-04 22:26:32 +0800

    fitur baru bro

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
-  ],
+  content: ["./resources/**/*.blade.php",
+  "./resources/**/*.js",
+  "./resources/**/*.vue",],
   theme: {
-    extend: {},
+    extend: {
+        colors: {
+        'regal-blue': '#243c5a',
+      },
+
+        // screens :{
+        //     sm: "440px",
+        //     md: "768px",
+        //     lg: "1024px",
+        // },
+    },
+    fontFamily :{
+        oswald :['Oswand','sans-serif'],
+        dmsans :['DM Sans','sans-serif']
+    }
   },
   plugins: [],
 }
